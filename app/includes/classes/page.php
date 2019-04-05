3b78edbbfc76cd7ffc696b0b7fd9493a6127f3e8
Daniel <voje@live.no>
2019-04-05 23:59:15 +0200

    Added search mood for period and readied AJAX
    Added a function that will be used when we calculate the average mood over a period of time, made two ajax-functions that might be used later, probably.

--- app/includes/classes/page.php
+++ app/includes/classes/page.php
@@ -10,18 +10,35 @@ class Page extends Db_object{
 	public $mood;
 	public $date;
 
-	public static function find_pages($user_id) {
+	public static function find_pages($user_id, $type, $date) {
 
 		global $database;
-		$room_id = $database->escape_string($room_id);
+		$user_id = $database->escape_string($user_id);
 
 		$sql = "SELECT * FROM " . self::$db_table . " WHERE ";
 		$sql .= "user_id = '{$user_id}' ";
-		$sql .= "ORDER BY id";
+		$sql .= "ORDER BY date";
 
 		return static::find_by_query($sql);
 
 	}
+
+	/**
+	 * The method that collects the average in mood between
+	 * a start-date and a end-date.
+	 */
+	public static function find_mood_in_period($startdate, $enddate) {
+
+		global $database;
+		$usre_id = $database->escape_string($user_id);
+
+		$sql = "SELECT AVG(mood) FROM " . self::$db_table . " WHERE ";
+		$sql .= "date BETWEEN '{$startdate}' AND '{$enddate}'";
+
+		return static::find_by_query($sql);
+
+	}
+
 
 } // End of Page-class
 
--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php include("app/includes/views/header.php"); ?>
 
 
+
+
+
+
+    <input type="button" value="Select File" onclick="find_file();">
+    <input type="file" id="selectfile">
+
+
+
+
+
 <?php include("app/includes/views/footer.php"); ?>
