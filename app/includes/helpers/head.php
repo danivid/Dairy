a6fa7d813d40e4cd0ddc3c41b27b6a9b324021c0
David Naist Øvernes <David.Naist@gmail.com>
2019-04-05 23:26:25 +0200

    Changed view structure

--- app/includes/helpers/head.php
+++ app/includes/helpers/head.php
@@ -1,3 +1,4 @@
+<head>
     <!-- Character set -->
     <meta charset="utf-8">
 
@@ -7,10 +8,16 @@
     <!-- Setting the viewport -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Ajax -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    
-    <title>Game-Site</title>
+    <!-- Title -->
+    <title>
+    <?php
+        if (true) {
+            echo true ? "Diary | Welcome" : "Diary | Home";
+        } else if (false) {
+            echo "Diary | Feelings";
+        }
+    ?>
+    </title>
 
     <!-- Page description -->
     <meta name="description" content="Description of the page less than 150 characters">
@@ -19,3 +26,4 @@
     <link rel="icon" type="assets/image/png" href="favicon.png">
 
     <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js" integrity="sha384-0pzryjIRos8mFBWMzSSZApWtPl/5++eIfzYmTgBBmXYdhvxPc+XcFEk+zJwDgWbP" crossorigin="anonymous"></script>
+</head>
--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<?php include("app/includes/views/header.php"); ?>
+<?php require_once("app/includes/init.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<?php include("app/includes/helpers/head.php") ?>
+<body>
+    <main>
+        <?php include("app/includes/views/navbar.php") ?>
+        <?php // true ? include("app/includes/views/navbar.php") : include("app/includes/views/navbar.php") ?>
+        <?php // true && include("app/includes/views/filter.php") ?>
+    <main>
+
+    <?php include("app/includes/views/footer.php") ?>
+</body>
+</html>
 
 
-<?php include("app/includes/views/footer.php"); ?>
+
