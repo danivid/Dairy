70c11cc21a54a8cd83e0365183da70340c5649c7
Daniel <voje@live.no>
2019-04-06 07:45:05 +0200

    Smileface
    On the nav

--- app/includes/views/navbar.php
+++ app/includes/views/navbar.php
@@ -3,36 +3,45 @@
         <!-- IF ON THIS PAGE -->
         <a <?php echo false ? "" : "href=\"index.php\"" ?> class="home-icon" ><?php echo true ? "<i class=\"fas fa-fw fa-book\"></i>" : "<i class=\"fas fa-fw fa-book-open\"></i>" ?></a>
 
+        <?php 
+         $rounded_avg = 0;
+        if ($session->is_signed_in()) {
+            $user_id = $session->user_id;
+            $avg = Page::find_mood_average(1);
+            $rounded_avg = round($avg);
+            echo $rounded_avg;
+        } ?>
+
         <!-- IF ON THIS PAGE -->
         <a <?php echo false ? "" : "href=\"mood.php\"" ?>
             class="mood-icon
             <?php
                 // DEPENDING ON MOOD LEVEL ROUNDED TO CLOSEST
-                if (true) {
-                    echo ' -very-happy';
-                } else if (true) {
-                    echo ' -happy';
-                } else if (true) {
-                    echo ' -neutral';
-                } else if (true) {
-                    echo ' -sad';
-                } else if (true) {
-                    echo ' -very-sad';
+                if ($rounded_avg = 5) {
+                    echo " -very-happy";
+                } else if ($rounded_avg =4) {
+                    echo " -happy";
+                } else if ($rounded_avg = 3 || $rounded_avg = 0) {
+                    echo " -neutral";
+                } else if ($rounded_avg = 2) {
+                    echo " -sad";
+                } else if ($rounded_avg = 1) {
+                    echo " -very-sad";
                 }
             ?>
             "
         >
             <!-- DEPENDING ON MOOD LEVEL ROUNDED TO CLOSEST -->
             <?php
-                if (true) {
+                if ($rounded_avg == 5) {
                     echo "<i class=\"fas fa-fw fa-laugh-beam\"></i>";
-                } else if (true) {
+                } else if ($rounded_avg ==4) {
                     echo "<i class=\"fas fa-fw fa-smile\"></i>";
-                } else if (true) {
+                } else if ($rounded_avg ==3) {
                     echo "<i class=\"fas fa-fw fa-meh\"></i>";
-                } else if (true) {
+                } else if ($rounded_avg == 2) {
                     echo "<i class=\"fas fa-fw fa-frown\"></i>";
-                } else if (true) {
+                } else if ($rounded_avg == 1) {
                     echo "<i class=\"fas fa-fw fa-sad-cry\"></i>";
                 }
             ?>
@@ -42,7 +51,7 @@
     <div id="sign" class="sign">
         <!-- IF SIGNED IN -->
 
-        <div class="sign-icon modal"><?php echo $session->is_signed_in() ? "<i class=\"fas fa-fw fa-sign-in-alt\"></i>" : "<i class=\"fas fa-fw fa-sign-out-alt\"></i>" ?></div>
+        <div class="sign-icon"><?php echo $session->is_signed_in() ? "<i class=\"fas fa-fw fa-sign-in-alt\"></i>" : "<i class=\"fas fa-fw fa-sign-out-alt\"></i>" ?></div>
   
     </div>
 </div>
