ac8e26a30774b844b3862d4d062df5aff4029e39
David Naist Øvernes <David.Naist@gmail.com>
2019-04-06 06:49:21 +0200

    Quick add: Modul saker

---
+++ app/includes/views/modal.php
@@ -0,0 +1,27 @@
+<div class="modal">
+    <div class="modal-frame">
+        <form action="post" class="form">
+            <div class="modal-header">
+                <div>Welcome!</div>
+                <div class="close"></div>
+            </div>
+
+            <div class="modal-body">
+                <div>
+                    <label for="username">Username</label>
+                    <input type="text" id="username">
+                </div>
+
+                <div>
+                    <label for="password">Password</label>
+                    <input type="text" id="password">
+                </div>
+
+                <div class="form-buttons">
+                    <button class="button -clear">Register</button>
+                    <button>Sign in</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
--- app/index.php
+++ app/index.php
@@ -5,30 +5,7 @@
 <?php include("includes/helpers/head.php") ?>
 <body>
     <main>
-        <div class="modal -show">
-            <div class="modal-content">
-                <form action="post" class="form">
-                    <div class="close">
-                        <div class="close-icon"></div>
-                    </div>
-
-                    <div>
-                        <label for="username">Username</label>
-                        <input type="text" id="username">
-                    </div>
-
-                    <div>
-                        <label for="password">Password</label>
-                        <input type="text" id="password">
-                    </div>
-
-                    <div class="form-buttons">
-                        <button class="button -clear">Register</button>
-                        <button>Sign in</button>
-                    </div>
-                </form>
-            </div>
-        </div>
+        <?php include("includes/views/modal.php") ?>
 
         <?php include("includes/views/navbar.php") ?>
 
